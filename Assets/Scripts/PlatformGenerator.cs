339a9e81f27419816f616fb692261dfaf3d32cc5
Сергей <kalininserdzhi@gmail.com>
2020-07-16 20:51:25 +0300

    clean code

--- Assets/Scripts/PlatformGenerator.cs
+++ Assets/Scripts/PlatformGenerator.cs
@@ -5,34 +5,29 @@ using UnityEngine;
 public class PlatformGenerator : MonoBehaviour
 {
     public GameObject platformPrefab;
-
-    public int numberOfPlatform = 20;
-    public float levelWidth = 3f;
-    public float minY = .2f;
-    public float maxY = 1.5f;
+    public int numberOfPlatform;
+    public float levelWidth;
+    public float minY;
+    public float maxY;
     private Vector2 lastPlatform;
     public Transform Player;
     private Vector2 spawnPosition = new Vector2();
-    
-    void Start()
-    {       
-        Spawn();
-    }
+    private void Start() => Spawn();
     private void Update()
     {
-        if ((lastPlatform.y -15) < Player.position.y ) 
+        if ((lastPlatform.y - 15) < Player.position.y) 
         {
             Spawn();           
         }       
     }
-    void Spawn()
+    private void Spawn()
     {       
         for (int i = 0; i < numberOfPlatform; i++)
         {
             spawnPosition.y += Random.Range(minY, maxY);
             spawnPosition.x = Random.Range(-levelWidth, levelWidth);
             GameObject clone = Instantiate(platformPrefab, spawnPosition, Quaternion.identity);           
-            if (i == (numberOfPlatform -1)) 
+            if (i == (numberOfPlatform - 1)) 
             {
                 lastPlatform = new Vector2(spawnPosition.x, spawnPosition.y);               
             }
